982308617975a09a0c80932ef84a1144f612e058
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-22 11:39:44 -0400

    Completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+    def talk
+      puts "Hello World!"
+    end
+    
+    def walk
+      puts "The Person is walking"
+    end
+
+end
+
+yaneizy = Person.new
+yaneizy.talk
+yaneizy.walk
+
+#remember to use Class instead of def to define Person
+
+#yaneizy example I did puts out in the terminal
+
+
+# ruby lib/person.rb
